37dcfebe138246bcb38113f8bc80cdde57908863
Murat Guchetl <gmurka@gmail.com>
2019-02-06 19:28:49 +0300

    added code cleanup fixed - setup.py, license, etc.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     version='.'.join(map(str, __import__('nodes').VERSION)),
 
-    description = 'A menus and navigation framework for Django framework.',
+    description='A menus and navigation framework for Django framework.',
     long_description=long_description,
 
     # The project's main homepage.
@@ -45,9 +45,11 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Database',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: BSD License',
@@ -55,6 +57,7 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
 
@@ -70,19 +73,21 @@ setup(
     # simple. Or you can use find_packages().
     #   packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     #   packages=['application',],
-    packages=['nodes',],
+    packages=find_packages(),
 
     # List run-time dependencies here. These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'django>=2.0.0',
+        'django>=2.0.0,<3.0.0',
     ],
 
-    python_requires='>=3.6, <4',
-
+    # Extras (optional features with their own dependencies)
     extras_require={},
+
+    python_requires='>=3.5,<4',
+
     include_package_data=True,
     zip_safe=False
 )
